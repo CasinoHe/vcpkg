33ffe8934795284f910083a94369e41303ffb4f1
casino <casinohe@outlook.com>
2023-06-05 15:45:27 +0800

    fix version of gzip, fix cannot create temp error

--- scripts/cmake/vcpkg_download_distfile.cmake
+++ scripts/cmake/vcpkg_download_distfile.cmake
@@ -176,9 +176,9 @@ If you do not know the SHA512, add it as 'SHA512 0' and re-run this command.")
 
     # Works around issue #3399
     # Delete "temp0" directory created by the old version of vcpkg
-    file(REMOVE_RECURSE "${DOWNLOADS}/temp0")
-    file(REMOVE_RECURSE "${DOWNLOADS}/temp")
-    file(MAKE_DIRECTORY "${DOWNLOADS}/temp")
+    # file(REMOVE_RECURSE "${DOWNLOADS}/temp0")
+    # file(REMOVE_RECURSE "${DOWNLOADS}/temp")
+    # file(MAKE_DIRECTORY "${DOWNLOADS}/temp")
 
     # check if file with same name already exists in downloads
     if(EXISTS "${downloaded_file_path}" AND NOT arg_ALWAYS_REDOWNLOAD)
